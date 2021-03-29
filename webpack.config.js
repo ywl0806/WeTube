428a9832be0cd487a750ef1e8e4f02d720567c9d
unknown <ywl0806@gmail.com>
2021-03-29 11:28:14 +0900

    I fixed it!!

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
-const autoprefixer = require("autoprefixer");
-const ExtractCSS = require("extract-text-webpack-plugin");
+// const autoprefixer = require("autoprefixer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
+
 const config = {
   entry: ENTRY_FILE,
   mode: MODE,
@@ -12,22 +13,33 @@ const config = {
     rules: [
       {
         test: /\.(scss)$/,
-        use: ExtractCSS.extract([
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
           {
             loader: "css-loader",
           },
           {
             loader: "postcss-loader",
             options: {
-              plugin() {
-                return [autoprefixer({ browsers: "cover 99.5%" })];
+              postcssOptions: {
+                plugins: [
+                  [
+                    "autoprefixer",
+                    {
+                      //options
+                      browsers: "cover 99.5%",
+                    },
+                  ],
+                ],
               },
             },
           },
           {
             loader: "sass-loader",
           },
-        ]),
+        ],
       },
     ],
   },
@@ -35,7 +47,24 @@ const config = {
     path: OUTPUT_DIR,
     filename: "[name].js",
   },
-  plugins: [new ExtractCSS("styles.css")],
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+    }),
+  ],
 };
 
 module.exports = config;
+
+/*
+          {
+            loader: "postcss-loader",
+            options: {
+              plugin() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
+              }
+            }
+          },
+*/
